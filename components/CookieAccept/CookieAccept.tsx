378b0190ef7ec5966708ad3c5511741697565ad8
tereshonkov <tereshonkov.dima@gmail.com>
2025-04-08 09:16:04 -0700

    cookie redirect

--- components/CookieAccept/CookieAccept.tsx
+++ components/CookieAccept/CookieAccept.tsx
@@ -21,12 +21,12 @@ export default function CookieAccept() {
             >
                 Zustimmen
             </button>
-            <button
+            <a href="/cookie"
                 onClick={handleAccept}
                 className="mt-2 bg-emerald-600 text-white px-4 py-2 rounded cursor-pointer"
             >
                 Ablehnen
-            </button>
+            </a>
             </div>
             </div>
         )}
